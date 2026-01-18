5f5a1ad44bac2af44481bdbe048a5d788379a2f3
Hermes <chavezhermes3@gmail.com>
2026-01-17 21:16:05 -0500

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,20 +12,32 @@ public class Main {
         System.out.print("$ ");
         Scanner input = new Scanner(System.in);
         String command = input.nextLine();
+
         ArrayList<String> shellCommands = new ArrayList<>(List.of("echo"));
 
         if (!command.equals("exit")) {
-            if(shellCommands.contains(command)) {
-                executeShellCommand(command);
+            String[] result = checkCommand(command);
+            if(shellCommands.contains(result[0])) {
+                executeShellCommand(result);
             }else {
                 System.out.println(command + ": command not found");
                 startShellCommand();
             }
         }
     }
-    public static void executeShellCommand(String command) {
+    public static String[] checkCommand(String command) {
+        String[] parts = command.split("\\s+", 2);
+        String[] result = new String[2];
+
+        String cmd = parts[0];                 // before first space
+        String args = (parts.length > 1) ? parts[1] : "";   // everything after
+        result[0] = cmd;
+        result[1] = args;
+        return result;
+    }
+    public static void executeShellCommand(String[] command) {
         if (command.equals("echo")) {
-            echoCommand(command);
+            echoCommand(command[1]);
         }
     }
     public static void echoCommand(String command) {
