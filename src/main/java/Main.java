fac513a07a68c492b910378cf66ef298a598627d
Hermes <chavezhermes3@gmail.com>
2026-01-17 20:40:56 -0500

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-         System.out.print("$ ");
-         Scanner input = new Scanner(System.in);
-        String Command = input.nextLine();
-        System.out.println(Command + ": command not found");
+       startShellCommand();
+        //System.out.println(Command + ": command not found");
     }
+    public static void startShellCommand() {
+        System.out.print("$ ");
+        Scanner input = new Scanner(System.in);
+        String command = input.nextLine();
+        if (!command.equals("exit")) {
+            System.out.println(command + ": command not found");
+            startShellCommand();
+        }
+    }
+
 }
